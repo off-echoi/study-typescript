fabb2c3c7bd64b356a89a7d87ce2482ba756376d
ecchoi <off.echoi@gmail.com>
2021-05-10 00:08:19 +0900

    기본타입

---
+++ 1-types/1-1.basic.ts
@@ -0,0 +1,28 @@
+{
+    /**
+     * JavaScript
+     * Primitive: number, string, boolean, bigint, symbol, null, undefined
+     * Object: function, array ...
+     */
+    // number
+    const num: number = -1
+
+    //string
+    const str: string = "hi"
+    
+    //boolean
+    const boal:boolean = false
+
+    // undefined
+    let name: undefined // 💩
+    let age: number | undefined
+    age = 0
+    age = undefined
+    function find():number | undefined {
+        return undefined
+    }
+    //null
+    let person: null // 💩
+    let people:string|null
+
+}
